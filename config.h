939dd699b452f3d561e9a270bc04514a6506bf5b
Alexandr Chernakov <alexandr.chernakov@gmail.com>
2014-09-03 01:35:18 +0400

    Added annotations to configuration

--- config.h
+++ config.h
@@ -1,10 +1,19 @@
 #pragma once
 
+//Square size
 #define SQUARE_SZ 48
+
+//Position of the top left corner of the game field
 #define FIELD_OFFSET_X 32
 #define FIELD_OFFSET_Y 32
+
+//Size of field, in squares
 #define FIELD_H 20
 #define FIELD_W 10
+
+//Brick matrix size. Have to be 4 for classic game
 #define BRICK_SZ 4
+
+//Position of the field, where the next block is displayed
 #define NEXT_BRICK_X 520
 #define NEXT_BRICK_Y 240
